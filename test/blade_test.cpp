a549e730e9fd43c65f535226e0d2661c014fd2fb
Oliver Lee <oliverzlee@gmail.com>
2024-08-29 22:02:16 +0200

    define canonical form for `blade` types (#4)
    Change-Id: If2396afbe3d57b84214445eb6728e660dc6c313a

--- test/blade_test.cpp
+++ test/blade_test.cpp
@@ -10,7 +10,7 @@ auto main() -> int
 
   using rga = ::rigid_geometric_algebra::algebra<double, 2>;
 
-  "blade constructible"_test = [] {
+  "constructible"_test = [] {
     const auto a = rga::blade<>{};
     const auto b = rga::blade<0>{0};
     const auto c = rga::blade<1>{1};
@@ -21,7 +21,7 @@ auto main() -> int
         eq(1, c.coefficient) and eq(2, d.coefficient));
   };
 
-  "blade comparable"_test = [] {
+  "comparable"_test = [] {
     const auto a = rga::blade<>{};
     const auto b = rga::blade<>{};
     const auto c = rga::blade<>{1};
@@ -29,7 +29,7 @@ auto main() -> int
     return expect(eq(a, b) and ne(a, c) and ne(b, c));
   };
 
-  "blade grade"_test = [] {
+  "grade"_test = [] {
     return expect(
         eq(0, rga::blade<>::grade) and      //
         eq(1, rga::blade<0>::grade) and     //
@@ -39,5 +39,51 @@ auto main() -> int
         eq(2, rga::blade<1, 2>::grade) and  //
         eq(2, rga::blade<0, 2>::grade) and  //
         eq(3, rga::blade<0, 1, 2>::grade));
+  };
+
+  "negatable"_test = [] {
+    return expect(eq(rga::blade<0, 1>{1}, -rga::blade<0, 1>{-1}));
+  };
+
+  "canonical form"_test = [] {
+    return expect(
+        eq(rga::blade<>{}, rga::blade<>{}.canonical()) and
+        eq(rga::blade<0>{}, rga::blade<0>{}.canonical()) and
+        eq(rga::blade<1>{}, rga::blade<1>{}.canonical()) and
+        eq(rga::blade<2>{}, rga::blade<2>{}.canonical()) and
+        eq(rga::blade<0, 1>{}, rga::blade<0, 1>{}.canonical()) and
+        eq(rga::blade<0, 1>{}, rga::blade<1, 0>{}.canonical()) and
+        eq(rga::blade<0, 2>{}, rga::blade<0, 2>{}.canonical()) and
+        eq(rga::blade<0, 2>{}, rga::blade<2, 0>{}.canonical()) and
+        eq(rga::blade<0, 1, 2>{}, rga::blade<0, 2, 1>{}.canonical()) and
+        eq(rga::blade<0, 1, 2>{}, rga::blade<1, 2, 0>{}.canonical()) and
+        eq(rga::blade<0, 1, 2>{}, rga::blade<0, 1, 2>{}.canonical()));
+  };
+
+  "sign conversion for canonical form"_test = [] {
+    return expect(
+        eq(rga::blade<0, 1>{1}, -rga::blade<1, 0>{1}) and
+        eq(rga::blade<0, 1, 2>{1}, rga::blade<0, 1, 2>{1}) and
+        eq(rga::blade<0, 1, 2>{1}, rga::blade<1, 2, 0>{1}) and
+        eq(rga::blade<0, 1, 2>{1}, rga::blade<2, 0, 1>{1}) and
+        eq(rga::blade<0, 1, 2>{1}, -rga::blade<0, 2, 1>{1}) and
+        eq(rga::blade<0, 1, 2>{1}, -rga::blade<1, 0, 2>{1}) and
+        eq(rga::blade<0, 1, 2>{1}, -rga::blade<2, 1, 0>{1}));
+  };
+
+  "sign conversion for canonical form 3D"_test = [] {
+    using rga3 = ::rigid_geometric_algebra::algebra<double, 3>;
+
+    return expect(
+        eq(rga3::blade<0, 1, 2, 3>{1}, rga3::blade<0, 1, 2, 3>{1}) and
+        eq(rga3::blade<0, 1, 2, 3>{1}, -rga3::blade<0, 1, 3, 2>{1}) and
+        eq(rga3::blade<0, 1, 2, 3>{1}, -rga3::blade<0, 2, 1, 3>{1}) and
+        eq(rga3::blade<0, 1, 2, 3>{1}, -rga3::blade<1, 0, 2, 3>{1}) and
+        eq(rga3::blade<0, 1, 2, 3>{1}, rga3::blade<0, 3, 1, 2>{1}) and
+        eq(rga3::blade<0, 1, 2, 3>{1}, rga3::blade<2, 0, 1, 3>{1}) and
+        eq(rga3::blade<0, 1, 2, 3>{1}, rga3::blade<0, 2, 3, 1>{1}) and
+        eq(rga3::blade<0, 1, 2, 3>{1}, rga3::blade<1, 2, 0, 3>{1}) and
+        eq(rga3::blade<0, 1, 2, 3>{1}, rga3::blade<1, 0, 3, 2>{1}) and
+        eq(rga3::blade<0, 1, 2, 3>{1}, rga3::blade<3, 2, 1, 0>{1}));
   };
 }
