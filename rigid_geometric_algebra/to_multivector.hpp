4ab98f69e9ed5f9b15233e561ca54c5efaee73b5
Oliver Lee <oliverzlee@gmail.com>
2024-09-10 16:27:19 -0700

    def `to_multivector` promotion, `blade` and `multivector` addition
    Change-Id: Ief3bd1f05f4c26620db75ad86a7a9d33fee8e6cc

---
+++ rigid_geometric_algebra/to_multivector.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "algebra_type.hpp"
+#include "rigid_geometric_algebra/is_blade.hpp"
+#include "rigid_geometric_algebra/multivector.hpp"
+
+#include <type_traits>
+#include <utility>
+
+namespace rigid_geometric_algebra {
+
+/// promote value to a `multivector`
+/// @tparam B cv-ref qualified `blade` type
+/// @tparam b `blade` value
+///
+/// Returns a `multivector` containing a single `blade`.
+///
+template <class B>
+  requires is_blade_v<std::remove_cvref_t<B>>
+constexpr auto to_multivector(B&& b)
+    -> multivector<
+        algebra_type_t<std::remove_cvref_t<B>>,
+        std::remove_cvref_t<B>>
+{
+  return {std::forward<B>(b)};
+}
+
+}  // namespace rigid_geometric_algebra
