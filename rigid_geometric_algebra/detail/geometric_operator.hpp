6368eaed51391520c4211eca20bd995adfce2604
Oliver Lee <oliverzlee@gmail.com>
2024-10-06 15:26:34 -0700

    add `wedge` overloads for geometric types
    Change-Id: I6d46cf44386421e9253fa6b23c945a770e844291

---
+++ rigid_geometric_algebra/detail/geometric_operator.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "rigid_geometric_algebra/algebra_type.hpp"
+#include "rigid_geometric_algebra/detail/geometric_interface.hpp"
+#include "rigid_geometric_algebra/geometric_fwd.hpp"
+#include "rigid_geometric_algebra/is_multivector.hpp"
+
+#include <concepts>
+#include <type_traits>
+
+namespace rigid_geometric_algebra::detail {
+
+/// function object to convert a `multivector` to a geometric type
+///
+template <template <class> class to>
+class to_geometric_type_fn
+{
+public:
+  template <detail::multivector V>
+    requires std::constructible_from<to<algebra_type_t<V>>, V>
+  static constexpr auto operator()(V&& v) -> to<algebra_type_t<V>>
+  {
+    return to<algebra_type_t<V>>{std::forward<V>(v)};
+  }
+};
+
+/// function object to convert a `multivector` to one of multiple geometric
+/// types
+///
+/// @{
+
+template <template <class> class... to>
+class to_geometric_fn : public to_geometric_type_fn<to>...
+{
+public:
+  using to_geometric_type_fn<to>::operator()...;
+};
+
+inline constexpr auto to_geometric = to_geometric_fn<point, line>{};
+
+/// @}
+
+/// defines a geometric overload from `multivector` overloads
+///
+class geometric_operator
+{
+public:
+  template <class Self, detail::geometric T1, detail::geometric T2>
+    requires std::is_invocable_v<
+        decltype(to_geometric),
+        std::invoke_result_t<
+            const Self&,
+            decltype(std::declval<T1>().multivector()),
+            decltype(std::declval<T2>().multivector())>>
+  constexpr auto
+  operator()(this const Self& self, T1&& t1, T2&& t2) -> decltype(to_geometric(
+      self(std::forward<T1>(t1).multivector(),
+           std::forward<T2>(t2).multivector())))
+  {
+    return to_geometric(self(
+        std::forward<T1>(t1).multivector(),
+        std::forward<T2>(t2).multivector()));
+  }
+};
+
+}  // namespace rigid_geometric_algebra::detail
