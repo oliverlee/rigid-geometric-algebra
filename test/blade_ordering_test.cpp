be6824aab490f9c81915772c7cfff2913dbaeb29
Oliver Lee <oliverzlee@gmail.com>
2024-10-08 16:51:01 -0700

    fix blade order
    Change-Id: If6aec4167cd5f6780c609cae333da7fad954d268

--- test/blade_ordering_test.cpp
+++ test/blade_ordering_test.cpp
@@ -4,10 +4,14 @@
 #include <cstddef>
 
 using G2 = ::rigid_geometric_algebra::algebra<double, 2>;
+using G3 = ::rigid_geometric_algebra::algebra<double, 3>;
 using ::rigid_geometric_algebra::blade_ordering;
 
 template <std::size_t... Is>
 constexpr auto ord = blade_ordering<G2>{G2::blade<Is...>::dimension_mask};
+
+template <std::size_t... Is>
+constexpr auto ord3 = blade_ordering<G3>{G3::blade<Is...>::dimension_mask};
 
 auto main() -> int
 {
@@ -28,4 +32,6 @@ auto main() -> int
         lt(ord<>, ord<2>) and lt(ord<0, 1>, ord<0, 2>) and
         lt(ord<0, 2>, ord<1, 2>) and lt(ord<1, 2>, ord<0, 1, 2>));
   };
+
+  "lt"_test = [] { return expect(lt(ord3<0, 3>, ord3<1, 2>)); };
 }
