ec8cb82a31238a9b22727e3e5aacfbf99b9d1ded
Oliver Lee <oliverzlee@gmail.com>
2024-10-08 23:50:26 -0700

    use homogeneous coordinates for `geom::Point` (#106)
    Change-Id: I42f1fb28eee312def60de79e4b670ac6aeedf2ad

--- test/points_example.cpp
+++ test/points_example.cpp
@@ -4,12 +4,11 @@
 #include <iostream>
 #include <vector>
 
-using G2 = ::rigid_geometric_algebra::algebra<double, 2>;
+using G3 = ::rigid_geometric_algebra::algebra<double, 3>;
 
 auto main() -> int
 {
-  const auto points = std::vector<G2::point>{
-      {1, 2, 3}, {2, 3, 4}, {3, 4, 5}, {4, 5, 6}, {5, 6, 7}};
+  const auto points = std::vector<G3::point>{{1, 1, 0, 0}, {1, 0, 1, 0}};
 
   std::cout << *glz::write_json(points) << "\n";
 }
